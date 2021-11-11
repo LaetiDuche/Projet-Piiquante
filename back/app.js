70b38c1483ad3f1f38ec36594240a3085d00e35a
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-11 17:26:20 +0100

    maj app

--- back/app.js
+++ back/app.js
@@ -17,42 +17,14 @@ app.use((req, res, next) => {
 
 //-----------
 const Sauce = require('./models/Sauce');
-app.post('/api/sauce', (req, res, next) => {
-  delete req.body._id;
-  const sauce = new Sauce({
-    ...req.body
-  });
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+const saucesRoutes = require('./routes/sauces');
 
-app.use('/api/sauce', (req, res, next) => {
-  Sauce.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
 
-app.get('/api/sauce/:id', (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.put('/api/sauce/:id', (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/sauce/:id', (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 
 
 //-------------
 app.use(express.json());
+app.use('/api/sauces', saucesRoutes);
+
 
 module.exports = app;

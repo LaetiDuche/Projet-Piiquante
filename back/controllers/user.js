f042457e120f2f1776d462f889a5ea5368de25d8
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-14 21:33:17 +0100

    modif token user

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res) => {
+exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
@@ -16,7 +16,7 @@ exports.signup = (req, res) => {
     .catch(error => res.status(500).json({ error }));
 };
 
-exports.login = (req, res) => {
+exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
@@ -31,7 +31,7 @@ exports.login = (req, res) => {
             userId: user._id,
             token: jwt.sign(
               { userId: user._id },
-              process.env.TOKEN,
+              'RANDOM_SECRET_KEY',
               { expiresIn: '24h' }
             )
           });

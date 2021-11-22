2160af2bac247b17146c1d775887731fc6fd3ced
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-22 12:38:58 +0100

    modif emplacement crud

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -1,5 +1,20 @@
+//CRUD
+
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
+
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+    next();
+};
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+};
 
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
@@ -43,19 +58,7 @@ exports.deleteSauce = (req, res, next) => {
 
 };
 
-exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-};
-
-exports.getAllSauces = (req, res) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-};
-
-exports.addLike = (req, res, next) => {
+exports.likeSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (!sauce) {

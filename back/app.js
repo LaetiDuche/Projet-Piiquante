cdd7dfdf2088c619742c573004e4c92d04097046
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-11-22 12:39:22 +0100

    modif require

--- back/app.js
+++ back/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({path: './.env'});
 const helmet = require('helmet');
 
 const saucesRoutes = require('./routes/sauces');
@@ -10,25 +10,29 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 mongoose.connect(process.env.SECRET_DB,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  app.use(helmet());
+app.use(helmet());
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-  
-  app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.urlencoded({ extended: true }));
 
-  app.use(express.json());
-  
-  app.use('/api/sauces', saucesRoutes);
-  app.use('/api/auth', userRoutes);
-  
-  module.exports = app;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(express.json());
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
